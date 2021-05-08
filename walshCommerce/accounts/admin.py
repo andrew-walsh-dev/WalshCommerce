7e4a3500dcc4be9d321c7f074a316053aa68508f
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-08 12:51:45 -0500

    added auto slug generation for categories and created first 5 categories

--- walshCommerce/accounts/admin.py
+++ walshCommerce/accounts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(Account)
+
+#CUSTOM ADMIN MODEL TO HASH USER PASSWORDS AND MAKE THEM READ-ONLY
+class AccountAdmin(UserAdmin):
+    list_display = (
+        'email', 
+        'first_name', 
+        'last_name', 
+        'username', 
+        'last_login', 
+        'date_joined',
+        'is_active'    
+    )
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined', )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)

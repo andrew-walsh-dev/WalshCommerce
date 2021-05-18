606de6fa573b3ddbe12bc0c8bfe1f3d7d5d67e32
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-18 14:19:41 -0500

    added first products and fixed django key security breach

--- walshCommerce/store/admin.py
+++ walshCommerce/store/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
 
 admin.site.register(Product, ProductAdmin)

6b48d3ff9b067cabb94f868a2b3d75a4e331557b
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-18 20:42:35 -0500

    render products to homepage preview

--- walshCommerce/walshCommerce/views.py
+++ walshCommerce/walshCommerce/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
 def homepage(req):
-    return render(req, 'index.html')
+    #grab all products, then filter out non-availables
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+    #pass in the list of products to render
+    return render(req, 'index.html', context)

285ebeeb08b8cc37a7a168c356f5faa5937e0031
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-19 21:03:02 -0500

    added filter by category for store products

--- walshCommerce/store/views.py
+++ walshCommerce/store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
+from category.models import Category
 
 # Create your views here.
-def store(req):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(req, category_slug=None):
+    categories = None
+    products = None
+
+    #allow products to be filtered and shown by category
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products': products,
